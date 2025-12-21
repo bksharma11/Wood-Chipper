3f1ea1f58e30a03b0e7f78d8097d87051a14c0f3
bksharma11 <159747564+bksharma11@users.noreply.github.com>
2025-12-21 16:50:26 +0530

    Add files via upload

---
+++ components/Footer.tsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="fixed bottom-0 left-0 right-0 bg-[#0a0a0a] border-t border-slate-700/30 p-3 z-50 flex justify-center items-center shadow-[0_-5px_20px_rgba(0,0,0,0.5)]">
+      <span className="gold-neon text-xl">
+        Design by 'B.K. SHARMA'
+      </span>
+    </footer>
+  );
+};
+
+export default Footer;
